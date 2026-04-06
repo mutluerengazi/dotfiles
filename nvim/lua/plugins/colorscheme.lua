584a23190633f9123100e93ee58c7ebde16ca985
erengazimutlu <mutluerengazi@proton.me>
2026-04-06 20:41:44 +0200

    update colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,30 @@
 -- lua/plugins/rose-pine.lua
 return {
-  'sainnhe/gruvbox-material',
+  "rebelot/kanagawa.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = false
-    vim.cmd.colorscheme('gruvbox-material')
+    require('kanagawa').setup({
+      compile = false,  -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = false },
+      functionStyle = {},
+      keywordStyle = { italic = false },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false,   -- do not set background color
+      dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = {             -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      theme = "wave",  -- Load "wave" theme
+      background = {   -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
+        light = "lotus"
+      },
+    })
+    vim.cmd.colorscheme('kanagawa')
   end
 }
