13bdc9e428d9d6959dd7a7c16d91cfb67baca2d8
erengazimutlu <mutluerengazi@proton.me>
2026-04-16 00:14:56 +0200

    update colors

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,7 @@
--- lua/plugins/rose-pine.lua
--- Using Lazy
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
+  "Mofiqul/dracula.nvim",
+  priority = 1000,
   config = function()
-    require('onedark').setup {
-      style = 'darker'
-    }
-    require('onedark').load()
+    require('dracula').load()
   end
 }
