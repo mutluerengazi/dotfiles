e9f10c0c39d766844d2de2ca948ee26129505d88
erengazimutlu <mutluerengazi@proton.me>
2026-04-15 22:34:22 +0200

    update colors

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,30 +1,12 @@
 -- lua/plugins/rose-pine.lua
+-- Using Lazy
 return {
-  "rebelot/kanagawa.nvim",
-  lazy = false,
-  priority = 1000,
+  "navarasu/onedark.nvim",
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require('kanagawa').setup({
-      compile = false,  -- enable compiling the colorscheme
-      undercurl = true, -- enable undercurls
-      commentStyle = { italic = false },
-      functionStyle = {},
-      keywordStyle = { italic = false },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false,   -- do not set background color
-      dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      colors = {             -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-      },
-      theme = "wave",  -- Load "wave" theme
-      background = {   -- map the value of 'background' option to a theme
-        dark = "wave", -- try "dragon" !
-        light = "lotus"
-      },
-    })
-    vim.cmd.colorscheme('kanagawa')
+    require('onedark').setup {
+      style = 'darker'
+    }
+    require('onedark').load()
   end
 }
