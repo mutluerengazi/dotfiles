6a3fccfa2660f4f0abb10f6ebefdb9d3a5b356fc
erengazimutlu <mutluerengazi@proton.me>
2026-06-11 23:31:06 +0200

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  priority = 1000,
-  config = function()
-    require('rose-pine').setup({ variant = 'dawn' })
-    vim.cmd('colorscheme rose-pine-dawn')
-  end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({ flavour = "latte", styles = { italics = false } })
+		vim.cmd("colorscheme catppuccin-latte")
+	end,
 }
