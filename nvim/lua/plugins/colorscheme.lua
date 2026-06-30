03d00f0750676824e2ac16d5de507fa183255baf
erengazimutlu <mutluerengazi@proton.me>
2026-06-30 22:59:32 +0200

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,10 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	priority = 1000,
-	config = function()
-		require("rose-pine").setup({
-			variant = "dawn",
-			styles = { italic = false },
-		})
-		vim.cmd("colorscheme rose-pine")
-	end,
+    "sainnhe/everforest",
+    priority = 1000,
+    config = function()
+        vim.g.everforest_background = "medium"
+        vim.g.everforest_enable_italic = 0
+        vim.g.everforest_disable_italic_comment = 1
+        vim.cmd("colorscheme everforest")
+    end,
 }
