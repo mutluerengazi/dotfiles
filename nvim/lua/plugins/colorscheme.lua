d1270443bc55cf350e3772f97702c55b4f5021e5
erengazimutlu <mutluerengazi@proton.me>
2026-07-09 22:08:36 +0200

    updt

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,28 @@
 return {
-    "sainnhe/everforest",
-    priority = 1000,
-    config = function()
-        vim.g.everforest_background = "medium"
-        vim.g.everforest_enable_italic = 0
-        vim.g.everforest_disable_italic_comment = 1
-        vim.cmd("colorscheme everforest")
-    end,
+	"rebelot/kanagawa.nvim",
+	priority = 1000,
+	config = function()
+		require("kanagawa").setup({
+			compile = false, -- enable compiling the colorscheme
+			undercurl = true, -- enable undercurls
+			commentStyle = { italic = false },
+			functionStyle = {},
+			keywordStyle = { italic = false },
+			statementStyle = { bold = false },
+			typeStyle = {},
+			transparent = false, -- do not set background color
+			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+			colors = { -- add/modify theme and palette colors
+				palette = {},
+				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+			},
+			theme = "lotus",
+			background = { -- map the value of 'background' option to a theme
+				dark = "wave", -- try "dragon" !
+				light = "lotus",
+			},
+		})
+		vim.cmd("colorscheme kanagawa")
+	end,
 }
