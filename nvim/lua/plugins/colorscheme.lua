3c397eaa3fe219a97b9ffca054d3bc4c12e97e78
erengazimutlu <mutluerengazi@proton.me>
2026-07-09 22:47:11 +0200

    updt

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,31 @@
 return {
-	"rebelot/kanagawa.nvim",
+	"navarasu/onedark.nvim",
+
 	priority = 1000,
+
 	config = function()
-		require("kanagawa").setup({
-			compile = false, -- enable compiling the colorscheme
-			undercurl = true, -- enable undercurls
-			commentStyle = { italic = false },
-			functionStyle = {},
-			keywordStyle = { italic = false },
-			statementStyle = { bold = false },
-			typeStyle = {},
-			transparent = false, -- do not set background color
-			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			colors = { -- add/modify theme and palette colors
-				palette = {},
-				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+		require("onedark").setup({
+			style = "light", -- options: dark, darker, cool, deep, warm, warmer, light
+
+			transparent = false,
+
+			term_colors = true,
+
+			code_style = {
+				comments = "none",
+				keywords = "none",
+				functions = "none",
+				strings = "none",
+				variables = "none",
 			},
-			theme = "lotus",
-			background = { -- map the value of 'background' option to a theme
-				dark = "wave", -- try "dragon" !
-				light = "lotus",
+
+			diagnostics = {
+				darker = false,
+				undercurl = true,
+				background = false,
 			},
 		})
-		vim.cmd("colorscheme kanagawa")
+
+		require("onedark").load()
 	end,
 }
