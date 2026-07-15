20e0e8ce0f9361ba10936b3203e1375765d6d9b6
erengazimutlu <mutluerengazi@proton.me>
2026-07-15 12:12:41 +0200

    updt

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,11 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"projekt0n/github-nvim-theme",
+	name = "github-theme",
+	lazy = false,
 	priority = 1000,
 
 	config = function()
-		require("catppuccin").setup({
-			flavour = "latte",
-			no_italic = true,
-			styles = {
-				comments = {},
-				conditionals = {},
-				loops = {},
-				functions = {},
-				keywords = {},
-				strings = {},
-				variables = {},
-				numbers = {},
-				booleans = {},
-				properties = {},
-				types = {},
-				operators = {},
-			},
-		})
-
-		vim.cmd.colorscheme("catppuccin-latte")
+		require("github-theme").setup()
+		vim.cmd.colorscheme("github_dark_default")
 	end,
 }
